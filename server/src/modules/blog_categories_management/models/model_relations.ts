80d9966e696742ed4e815ffbc3bdb9c23af3d7f4
ProgrammerShahJalal <bestwebdesignerr@gmail.com>
2025-01-04 13:18:32 +0600

    blog view completed

--- server/src/modules/blog_categories_management/models/model_relations.ts
+++ server/src/modules/blog_categories_management/models/model_relations.ts
@@ -3,15 +3,17 @@ import Models from "../../../database/models";
 export function init() {
     const models = Models.get();
 
+    // Define the association with BlogModel
     models.BlogCategoriesModel.belongsToMany(models.BlogModel, {
         as: "category_blogs",
-        through:"blog_category_blog",
+        through: "blog_category_blog",
         otherKey: "blog_category_id",
-        foreignKey: "blog_id"
-    })
-    // models.ContactModel.hasOne(models.ContactModel2, {
-    //     as: "contacts2",
-    //     foreignKey: "contact_id",
-    //     sourceKey: "id",
-    // })
+        foreignKey: "blog_id",
+    });
+
+    // // Define the association with BlogViewModel
+    // models.BlogCategoriesModel.hasMany(models.BlogViewModel, {
+    //     foreignKey: "blog_category_id",
+    //     as: "views",
+    // });
 }
--- server/src/modules/blog_view_management/models/model_relations.ts
+++ server/src/modules/blog_view_management/models/model_relations.ts
@@ -2,10 +2,5 @@ import Models from "../../../database/models";
 
 export function init() {
     const models = Models.get();
-
-    // models.BlogModel.hasMany(models.BlogViewModel, {
-    //     foreignKey: "blog_id",
-    //     as: "views",
-    // })
     
 }
