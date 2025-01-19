9bc9e8fb0dd5c3b0d7cf67caf71a6d1886c97358
ProgrammerShahJalal <bestwebdesignerr@gmail.com>
2025-01-19 13:31:05 +0600

    event session assesment management

--- server/src/modules/event_faqs_management/models/model_relations.ts
+++ server/src/modules/event_faqs_management/models/model_relations.ts
@@ -3,7 +3,7 @@ import Models from "../../../database/models";
 export function init() {
     const models = Models.get();
 
-    models.EventModel.hasMany(models.EventCertifiedUsersModel, {
+    models.EventModel.hasMany(models.EventFaqsModel, {
         foreignKey: "event_id",
         as: "event_faqs",
     })
