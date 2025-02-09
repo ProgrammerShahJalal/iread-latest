c94b92c7211136eeac5168ac042b305da4cd1074
ProgrammerShahJalal <bestwebdesignerr@gmail.com>
2025-02-09 16:09:53 +0600

    authentication

---
+++ client/src/app/types/user.ts
@@ -0,0 +1,11 @@
+interface User{
+    id: number;
+    uid: string;
+    role_serial: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone_number: string;
+    photo: string;
+    slug: string;
+}
