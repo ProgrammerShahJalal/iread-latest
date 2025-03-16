f9c4bd00b6f44705699a79319fd173b639e8c4e6
ProgrammerShahJalal <you@example.com>
2025-03-16 23:47:02 +0600

    event enrollent user

--- server/public/management/super_admin/views/pages/event_attendance/Create.tsx
+++ server/public/management/super_admin/views/pages/event_attendance/Create.tsx
@@ -7,16 +7,26 @@ import { store } from './config/store/async_actions/store';
 import Input from './components/management_data_page/Input';
 import { initialState } from './config/store/inital_state';
 import { useSelector } from 'react-redux';
-import EventDropDown from "../events/components/dropdown/DropDown";
-import SessionDropDown from "../event_sessions/components/dropdown/DropDown";
-import UserDropDown from "../users/components/dropdown/DropDown";
+import EventDropDown from '../events/components/dropdown/DropDown';
+import SessionDropDown from '../event_sessions/components/dropdown/DropDown';
 import DateTime from '../../components/DateTime';
+import axios from 'axios';
 
-
-export interface Props { }
-
+export interface Props {}
+interface User {
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone_number: string;
+    slug: string;
+}
 
 const Create: React.FC<Props> = (props: Props) => {
+    const [selectedEventId, setSelectedEventId] = useState<number | null>(null);
+    const [users, setUsers] = useState<User[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
     const state: typeof initialState = useSelector(
         (state: RootState) => state[setup.module_name],
     );
@@ -32,7 +42,6 @@ const Create: React.FC<Props> = (props: Props) => {
         }
     }
 
-
     function get_value(key) {
         try {
             if (state.item[key]) return state.item[key];
@@ -43,6 +52,26 @@ const Create: React.FC<Props> = (props: Props) => {
         return '';
     }
 
+    useEffect(() => {
+        if (!selectedEventId) return;
+
+        const fetchUsers = async () => {
+            setLoading(true);
+            setError(null);
+            try {
+                const response = await axios.get(
+                    `http://127.0.0.1:5011/api/v1/event-enrollments/by-event/${selectedEventId}`,
+                );
+                setUsers(response.data.data);
+            } catch (err) {
+                setError('Failed to fetch users.');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchUsers();
+    }, [selectedEventId]);
 
     return (
         <>
@@ -55,47 +84,109 @@ const Create: React.FC<Props> = (props: Props) => {
                             className="mx-auto pt-3"
                         >
                             <div>
-
-                                <h5 className="mb-4">Event Attendance Informations</h5>
+                                <h5 className="mb-4">
+                                    Event Attendance Informations
+                                </h5>
                                 <div className="form_auto_fit">
-
                                     <div className="form-group form-vertical">
                                         <label>Events</label>
-                                        <EventDropDown name="events"
+                                        <EventDropDown
+                                            name="events"
                                             multiple={false}
                                             get_selected_data={(data) => {
-                                                console.log(data)
+                                                console.log(Number(data.ids));
+                                                setSelectedEventId(
+                                                    Number(data.ids),
+                                                );
                                             }}
                                         />
                                     </div>
                                     <div className="form-group form-vertical">
                                         <label>Sessions</label>
-                                        <SessionDropDown name="sessions"
+                                        <SessionDropDown
+                                            name="sessions"
                                             multiple={false}
                                             get_selected_data={(data) => {
-                                                console.log(data)
+                                                console.log(data);
                                             }}
                                         />
                                     </div>
-                                    <div className="form-group form-vertical">
-                                        <label>Users</label>
-                                        <UserDropDown name="users"
-                                            multiple={false}
-                                            get_selected_data={(data) => {
-                                                console.log(data)
-                                            }}
-                                        />
-                                    </div>
-                                    <div className="form-group form-vertical">
-                                            <DateTime
-                                                name={"date_time"}
-                                                value={get_value('date_time')}
-                                                handler={(data) => console.log('Date Time Changed', data)}
-                                            />
-                                        </div>
-                                   
-                                </div>
 
+                                    {/* Loading State */}
+                                    {loading && <p>Loading...</p>}
+                                    {error && (
+                                        <p className="text-red-500">{error}</p>
+                                    )}
+
+                                    {/* User Table */}
+                                    {users.length > 0 ? (
+                                        <table className="w-full border-collapse border border-gray-300 mt-4">
+                                            <thead>
+                                                <tr className="bg-gray-100">
+                                                    <th className="border p-2">
+                                                        #
+                                                    </th>
+                                                    <th className="border p-2">
+                                                        Full Name
+                                                    </th>
+                                                    <th className="border p-2">
+                                                        Email
+                                                    </th>
+                                                    <th className="border p-2">
+                                                        Phone Number
+                                                    </th>
+                                                    <th className="border p-2">
+                                                        Slug
+                                                    </th>
+                                                    <th className="border p-2">
+                                                        Attendence
+                                                    </th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {users.map((user, index) => (
+                                                    <tr
+                                                        key={index}
+                                                        className="text-center"
+                                                    >
+                                                        <td className="border p-2">
+                                                            {index + 1}
+                                                        </td>
+                                                        <td className="border p-2">{`${user.first_name} ${user.last_name}`}</td>
+                                                        <td className="border p-2">
+                                                            {user.email}
+                                                        </td>
+                                                        <td className="border p-2">
+                                                            {user.phone_number}
+                                                        </td>
+                                                        <td className="border p-2">
+                                                            {user.slug}
+                                                        </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    ) : (
+                                        !loading && (
+                                            <p>
+                                                No users found for this event.
+                                            </p>
+                                        )
+                                    )}
+
+                                    {/* <div className="form-group form-vertical">
+                                        <DateTime
+                                            name={'date_time'}
+                                            value={get_value('date_time')}
+                                            handler={(data) =>
+                                                console.log(
+                                                    'Date Time Changed',
+                                                    data,
+                                                )
+                                            }
+                                        />
+                                    </div> */}
+                                </div>
                             </div>
 
                             <div className="form-group form-vertical">
