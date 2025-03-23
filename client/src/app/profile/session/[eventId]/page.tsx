f7ae1d80a4d4e3339150f599ffb93e5da693e346
ProgrammerShahJalal <bestwebdesignerr@gmail.com>
2025-03-23 12:07:12 +0600

    event session and session assesments

--- client/src/app/profile/session/[eventId]/page.tsx
+++ client/src/app/profile/session/[eventId]/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import React, { useEffect, useState, use } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ProfileLayout from '../../../../components/ProfileLayout';
 import { useParams } from 'next/navigation';
-
+import { FaCalendarAlt, FaClock, FaListAlt, FaCheckCircle, FaBook } from 'react-icons/fa';
+import moment from 'moment/moment';
 
 // Define the type for a form field
 interface Session {
@@ -17,25 +18,36 @@ interface Session {
     total_time: string;
 }
 
+// Define the type for a form field
+interface SessionAssesment {
+    id: number;
+    event_id: number;
+    event_session_id: number;
+    title: string;
+    description: string;
+    mark: number;
+    pass_mark: number;
+    start: string;
+    end: string;
+}
+
 function EventSessionPage() {
-      const [user, setUser] = useState<User | null>(null);
+    const [user, setUser] = useState<User | null>(null);
     const [session, setSession] = useState<Session | null>(null);
-    const [eventFormFieldId, setEventFormFieldId] = useState(0);
+    const [sessionAssesment, setSessionAssesment] = useState<SessionAssesment | null>(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
-    const [formData, setFormData] = useState<{ [key: string]: string }>({});
 
-    const params = useParams<{ eventId: string;}>()
+    const params = useParams<{ eventId: string; }>()
     const eventId = Number(params?.eventId);
-
 
     useEffect(() => {
         const storedUser = localStorage.getItem("user");
         if (storedUser) {
-          const parsedUser = JSON.parse(storedUser);
-          setUser(parsedUser);
+            const parsedUser = JSON.parse(storedUser);
+            setUser(parsedUser);
         }
-      }, []);
+    }, []);
 
     const BASE_URL =
         process.env.NODE_ENV === "production"
@@ -46,12 +58,10 @@ function EventSessionPage() {
         const fetchSession = async () => {
             try {
                 const response = await axios.get(`${BASE_URL}/api/v1/event-sessions/event/${eventId}`);
-                console.log('res', response.data.data);
                 if (response.data.status === 200) {
                     setSession(response.data.data);
-                    setEventFormFieldId(response.data.data.id);
                 } else {
-                    setError('No data found');
+                    setError('No session data found');
                 }
             } catch (err: any) {
                 setError(err.message);
@@ -61,41 +71,47 @@ function EventSessionPage() {
         };
 
         fetchSession();
+        const fetchSessionAssesment = async () => {
+            try {
+                const response = await axios.get(`${BASE_URL}/api/v1/event-session-assesments/event/${eventId}`);
+                if (response.data.status === 200) {
+                    setSessionAssesment(response.data.data);
+                } else {
+                    setError('No session assesment data found');
+                }
+            } catch (err: any) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchSessionAssesment();
     }, [BASE_URL, eventId]);
 
-
-    // const handleSubmit = async (e: React.FormEvent) => {
-    //     e.preventDefault();
-    //     // Prepare the payload
-    //     const payload = {
-    //         event_id: eventId,
-    //         event_form_field_id: eventFormFieldId,
-    //         user_id: user?.id,
-    //     };
-
-    //     try {
-    //         const response = await axios.post(
-    //             `${BASE_URL}/api/v1/event-feedback-form-field-values/store`,
-    //             payload
-    //         );
-    //         if (response.data.status === 201) {
-    //             alert("Feedback submitted successfully!");
-    //         } else {
-    //             alert("Failed to submit feedback.");
-    //         }
-    //     } catch (err: any) {
-    //         console.error("Error submitting feedback:", err);
-    //         alert("An error occurred while submitting feedback.");
-    //     }
-    // };
-
     if (loading) {
-        return <div className='text-center mt-24 min-h-[100vh]'>Loading...</div>;
+        return (
+            <ProfileLayout>
+                <div className="flex justify-center items-center min-h-[100vh]">
+                    <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+                </div>
+            </ProfileLayout>
+        );
     }
 
     if (error) {
-        return <div className='min-h-[100vh] py-10 text-center'>Error: {error}</div>;
+        return (
+            <ProfileLayout>
+                <div className="flex justify-center items-center min-h-[100vh]">
+                    <div className="text-red-500 text-2xl">Error: {error}</div>
+                </div>
+            </ProfileLayout>
+        );
     }
+
+    let formatTime = (time: string) => {
+        return moment(time, 'HH:mm').format('h:mmA');
+    };
 
     return (
         <ProfileLayout>
@@ -104,7 +120,70 @@ function EventSessionPage() {
                     <h2 className="text-3xl font-bold text-gray-800">Event Session</h2>
                     <p className="my-1 text-gray-600">Explore the session regarding to the event</p>
                 </div>
-                {session && session.title}
+                {session ? (
+                    <div className="bg-white shadow-lg rounded-lg p-6 mb-8">
+                        <h3 className="text-2xl font-bold text-gray-800 mb-4">{session.title}</h3>
+                        <div className="space-y-4">
+                            <div className="flex items-center space-x-2">
+                                <FaListAlt className="text-gray-600" />
+                                <span className="text-gray-700">Topics: {session.topics}</span>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <FaCalendarAlt className="text-gray-600" />
+                                <span className="text-gray-700">Start: {formatTime(session.start)}</span>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <FaCalendarAlt className="text-gray-600" />
+                                <span className="text-gray-700">End: {formatTime(session.end)}</span>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <FaClock className="text-gray-600" />
+                                <span className="text-gray-700">Total Time: {session.total_time} Minutes</span>
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    <p className="text-center text-lg my-10">No Session Found</p>
+                )}
+
+                {sessionAssesment ? (
+                    <div className="bg-white shadow-lg rounded-lg p-6">
+                        <h3 className="text-2xl font-bold text-gray-800 mb-4">Session Assessment</h3>
+                        <div className="space-y-4">
+                            <div className="flex items-center space-x-2">
+                                <FaBook className="text-gray-600" />
+                                <span className="text-gray-700 text-2xl font-bold">{sessionAssesment.title}</span>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <FaListAlt className="text-gray-600" />
+                                <div className="post-content mt-10">
+                                    <div
+                                        className="post-content"
+                                        dangerouslySetInnerHTML={{ __html: sessionAssesment.description }}
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <FaCheckCircle className="text-gray-600" />
+                                <span className="text-gray-700">Marks: {sessionAssesment.mark}</span>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <FaCheckCircle className="text-gray-600" />
+                                <span className="text-gray-700">Pass Marks: {sessionAssesment.pass_mark}</span>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <FaCalendarAlt className="text-gray-600" />
+                                <span className="text-gray-700">Start: {formatTime(sessionAssesment.start)}</span>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <FaCalendarAlt className="text-gray-600" />
+                                <span className="text-gray-700">End: {formatTime(sessionAssesment.end)}</span>
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    <p className="text-center text-lg my-10">No Assessment Found</p>
+                )}
             </div>
         </ProfileLayout>
     );
